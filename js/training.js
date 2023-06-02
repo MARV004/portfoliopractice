ff73c6f7117de77e4ec10aef8c6fb71dc6aa58ba
Fernando Wertt <fer19_reta@hotmail.com>
2023-06-02 11:41:32 -0500

    First problems

---
+++ js/training.js
@@ -0,0 +1,16 @@
+// In this file we are going to write the problems. Let's keep all in english to practice that as well.
+
+// 1. Create a function that returns me an array of 10 names with alphabetically order.
+
+// 2. Given an array of numbers, I want the sum of all of those numbers:
+    // Example: [10, 6, 8] be equal 24.
+
+// 3. You have an array of person objects, where each object contains a name and age.
+// Your task is to calculate the average age of all the people in the array and return the result.
+    // Example: const people = [
+//   { name: 'John', age: 25 },
+//   { name: 'Jane', age: 30 },
+//   { name: 'Sam', age: 20 },
+//   { name: 'Alice', age: 35 },
+//   { name: 'Bob', age: 27 }
+// ];
